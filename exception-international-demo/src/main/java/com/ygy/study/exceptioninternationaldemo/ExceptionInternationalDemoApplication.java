053a78e8f4f7e1cb3d9b1db1457fe966399620c5
yangguangying <yangguangying@yy.com>
2020-06-27 23:23:08 +0800

    统一异常处理+国际化 demo

---
+++ exception-international-demo/src/main/java/com/ygy/study/exceptioninternationaldemo/ExceptionInternationalDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ygy.study.exceptioninternationaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionInternationalDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionInternationalDemoApplication.class, args);
+    }
+
+}
---
+++ exception-international-demo/src/test/java/com/ygy/study/exceptioninternationaldemo/ExceptionInternationalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygy.study.exceptioninternationaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionInternationalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
