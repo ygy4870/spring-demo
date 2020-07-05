bc5f14f32b5bf7c5eaff64d269fbe26709770df8
yangguangying <yangguangying@yy.com>
2020-07-05 21:13:04 +0800

    mybatis genertor demo

---
+++ mybatis-genertor-demo/src/main/java/com/ygy/study/mybatisgenertordemo/MybatisGenertorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ygy.study.mybatisgenertordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisGenertorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisGenertorDemoApplication.class, args);
+    }
+
+}
---
+++ mybatis-genertor-demo/src/test/java/com/ygy/study/mybatisgenertordemo/MybatisGenertorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygy.study.mybatisgenertordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisGenertorDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
