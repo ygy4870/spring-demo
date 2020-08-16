df58d91eac500770de02565146e9c2941f2e8a50
yangguangying <yangguangying@yy.com>
2020-08-16 21:57:35 +0800

    aop demo init

---
+++ aop-demo/src/test/java/com/ygy/study/aopdemo/AopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygy.study.aopdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
