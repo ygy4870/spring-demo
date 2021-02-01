c572ca0fb2cb6742a0bb1628bcb0009dac3495d3
ygy4870 <1601541388@qq.com>
2021-02-01 16:53:35 +0800

    binlog

--- mysql/src/main/java/com/ygy/study/mysql/binlog/BinlogClient.java
+++ mysql/src/main/java/com/ygy/study/mysql/binlog/BinlogClient.java
@@ -15,8 +15,13 @@ import java.io.IOException;
 public class BinlogClient {
 
     public static void main(String[] args) {
-//        BinaryLogClient client = new BinaryLogClient("127.0.0.1", 3306, "root", "123456");
-        BinaryLogClient client = new BinaryLogClient("127.0.0.1", 3306, "root", "123456");
+        String hostname = "127.0.0.1";
+        int port = 3306;
+        // schema设置，测试没生效
+        String schema = "test2";
+        String userName = "root";
+        String password = "123456";
+        BinaryLogClient client = new BinaryLogClient(hostname, port, schema, userName, password);
         EventDeserializer eventDeserializer = new EventDeserializer();
         eventDeserializer.setCompatibilityMode(
                 EventDeserializer.CompatibilityMode.DATE_AND_TIME_AS_LONG,
