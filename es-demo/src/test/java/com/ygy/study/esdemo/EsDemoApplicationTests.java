ea0b0316b1c8eb93a4ee18d26d386980a2855f19
ygy4870 <1601541388@qq.com>
2021-03-02 13:46:49 +0800

    es-demo init

---
+++ es-demo/src/test/java/com/ygy/study/esdemo/EsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygy.study.esdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
